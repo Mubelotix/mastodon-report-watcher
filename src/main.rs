c815d1af86cec9a8506fdc0f8bcc1ebe4f96da3d
Mubelotix <mubelotix@gmail.com>
2024-01-09 21:13:17 +0100

    New version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, thread::sleep, time::Duration};
 use minreq::get;
 use serde::Deserialize;
 use time::OffsetDateTime;
@@ -8,43 +8,83 @@ struct Report {
     action_taken: bool,
     #[serde(with = "time::serde::iso8601")]
     created_at: OffsetDateTime,
+    //  With more fields that are not relevant here
 }
 
-fn should_shutdown(token: &str) -> bool {
+#[derive(Debug)]
+enum Error {
+    Http(minreq::Error),
+    Json(serde_json::Error),
+    Api { code: i32, body: String },
+}
+
+impl From<minreq::Error> for Error {
+    fn from(error: minreq::Error) -> Self {
+        Error::Http(error)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(error: serde_json::Error) -> Self {
+        Error::Json(error)
+    }
+}
+
+fn should_shutdown(token: &str) -> Result<bool, Error> {
     let response = get("https://mastodon.insa.lol/api/v1/admin/reports")
         .with_header("Authorization", format!("Bearer {}", token))
-        .send()
-        .expect("Failed to send report request");
+        .send()?;
 
     if response.status_code != 200 {
         let body = response.as_str().unwrap_or_default();
-        panic!("Failed to get reports: {} {}", response.status_code, body);
+        return Err(Error::Api {
+            code: response.status_code,
+            body: body.to_string(),
+        });
     }
 
     let body = response.as_str().unwrap_or("[]");
-    let mut reports: Vec<Report> = serde_json::from_str(body).expect("Failed to parse reports");
+    let mut reports: Vec<Report> = serde_json::from_str(body)?;
     reports.retain(|report| !report.action_taken);
 
-    let now = time::OffsetDateTime::now_utc();
+    let now = OffsetDateTime::now_utc();
     for report in reports.iter_mut() {
         let duration = now - report.created_at;
         if duration.whole_hours() > 23 {
-            return true;
+            return Ok(true);
         }
-        println!("{} ago", duration);
     }
 
-    false
+    Ok(false)
+}
+
+fn shutdown() {
+    println!("Shutting down");
 }
 
 fn main() {
     let token = env::var("MASTODON_TOKEN").expect("Expected a token in the environment");
 
+    let mut retries = 0;
     loop {
-        if should_shutdown(&token) {
-            println!("Server should shut down!");
+        match should_shutdown(&token) {
+            Ok(should_shutdown) => {
+                retries = 0;
+                if should_shutdown {
+                    shutdown();
+                }
+            },
+            Err(e) => {
+                println!("Error: {e:?}");
+                if retries > 10 {
+                    println!("Too many retries.");
+                    shutdown()
+                }
+                sleep(Duration::from_secs(10));
+                retries += 1;
+            }
         }
-        
-        std::thread::sleep(std::time::Duration::from_secs(60*30));
+
+        sleep(Duration::from_secs(60*30));
     }
 }
